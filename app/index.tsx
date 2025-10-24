080651eaf75ed15bd28cf11dc47c9a8da46b95f8
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2025-10-24 19:28:17 +0530

    safe area provider setup in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaProvider
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <SafeAreaView>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
